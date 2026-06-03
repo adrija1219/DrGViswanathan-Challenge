6eeb58fc3b2a10d143cc5ac1c94a5c6052651f95
Adrija Mitra <mitraadrija571@gmail.com>
2026-06-03 12:03:11 +0530

    Add solution for counting bits in Day2(L-338).cpp
    Implement a solution for counting bits using a bottom-up approach.

---
+++ Day2(L-338).cpp
@@ -0,0 +1,20 @@
+// Day -2 leetcode 338 (Counting Bits)
+//Approach: Bottom up approach (bit manipulation)
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> result(n+1);
+        if(n==0)
+         return result;
+        result[0]= 0;
+        for(int i=0; i<=n; i++){
+            if(i%2 !=0){
+                result[i] = result[i/2]+1;
+            }else{
+                result[i] = result[i/2];
+            }
+        }
+        return result;
+    }
+};
