afc1d5db488ca110cc9f8ada396882f13677b621
Adrija Mitra <mitraadrija571@gmail.com>
2026-06-04 11:11:10 +0530

    Add levelOrder function for binary tree traversal
    Implement BFS for binary tree level order traversal.

---
+++ Day-3(L-102).cpp
@@ -0,0 +1,35 @@
+// Leetcode 102
+// Binary Tree Level Order Traversal
+// Approach - BFS 
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>output;
+        queue<TreeNode*>q;
+        q.push(root);
+
+        if(root==NULL)
+          return output;
+
+        while(!q.empty()){
+            vector<int>helper;
+            int n=q.size();
+
+            for(int i=0; i<n; i++){
+                TreeNode* node= q.front();
+                q.pop();
+                helper.push_back(node->val);
+
+                if(node->left)
+                q.push(node->left);
+
+                if(node->right)
+                q.push(node->right);
+            }
+            output.push_back(helper);
+        }
+        
+        return output;
+    }
+};
